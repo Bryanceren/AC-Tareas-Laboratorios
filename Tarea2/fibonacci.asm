695f01ac2acd5c836b38eca759d185aea543eeeb
Bryanceren <00139817@uca.edu.sv>
2020-04-26 14:23:39 -0600

    arreglando ejercicio3 parte 1

--- Tarea2/fibonacci.asm
+++ Tarea2/fibonacci.asm
@@ -6,7 +6,6 @@
     mov    ax, 16d
     mov    bx, 1d
     mov    cx, 1d
-    mov     eax, 255d
     mov     dx,0000h
 
     lupita:mov    [220h + bx], cx
@@ -17,30 +16,11 @@
     add     cx, dx
     mov     dx, [200h]
 
-    cmp     cx,eax
-    ja      lupita2
-
     cmp     ax,bx
     ja      lupita
 
     cmp     ax,bx
     je      lupita
-
-    
-
-    lupita2:mov    [220h  + bx + 0001h], cx
-    inc     bx
-    
-    mov     [200h],cx
-    add     cx, dx
-    mov     dx, [200h]
-
-    cmp     ax,bx
-    ja      lupita2
-
-    cmp     ax,bx
-    je      lupita2
-
 
 
       
