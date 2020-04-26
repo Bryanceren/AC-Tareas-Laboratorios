074c213b4f5762a218cf6727a466469f38b0a647
Bryanceren <37248707+Bryanceren@users.noreply.github.com>
2020-04-26 14:46:51 -0600

    Update ejercicio1.asm

--- Tarea2/ejercicio1.asm
+++ Tarea2/ejercicio1.asm
@@ -1,30 +1,4 @@
-    org    100h
-    
 
-section	.text
-	; Ejercicio 1
-	mov	cx, 0000h
-
-	mov	ax, 1d ;Primer numero
-	mov	bx, 1d ;Segundo numero
-	add	ax, bx 
-	mov	bx, 3d ;Tercer numero
-	add	ax, bx
-	mov	bx, 1d ;Cuarto numero
-	add	ax, bx
-	mov	bx, 7d ;Quinto numero
-	add	ax, bx
-	mov	bx, 5d ; Numero de notas
-	div	bx
-
-	mov	di, 0d
-	mov	cx, [len]
-
-write	mov	bl, [comnt+di]
-	mov	[di+200h], bl
-	inc	di
-
-	loop	write
 
 section.data
 
